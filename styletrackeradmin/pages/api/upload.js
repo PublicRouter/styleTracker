f5fbe2a29a9416668d36a8bc40e9ff4948985cbe
James Garinger <jamesg@Jamess-MacBook-Air.local>
2023-06-07 21:26:34 -0500

    parse product upload image in upload api route to store to s3 bucket

--- styletrackeradmin/pages/api/upload.js
+++ styletrackeradmin/pages/api/upload.js
@@ -1,4 +1,9 @@
 import multiparty from 'multiparty';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import fs from 'fs';
+import mime from 'mime-types';
+
+const bucketName = 'next-styletracker';
 
 export default async function handler(req, res) {
     const form = new multiparty.Form();
@@ -8,9 +13,43 @@ export default async function handler(req, res) {
             resolve({ fields, files });
         });
     });
+
     console.log('length:', files.file.length);
-    console.log("file: ", files.file)
-    res.json('ok')
+    console.log("file: ", files.file);
+
+    const client = new S3Client({
+        region: 'us-east-2',
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        }
+    });
+
+    const links = [];
+
+    for(const file of files.file) {
+
+        const ext = file.originalFilename.split('.').pop();
+        console.log({ext, file});
+        
+        const newFilename = Date.now() + '.' + ext;
+
+        await client.send(new PutObjectCommand({
+            Bucket: bucketName,
+            Key: newFilename,
+            Body: fs.readFileSync(file.path),
+            ACL: 'public-read',
+            ContentType: mime.lookup(file.path),
+        }));
+
+        const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+        links.push(link);
+    
+    }
+
+   
+
+    return res.json({links});
 };
 
 export const config = {
