9e8b044a66bc1475bf0fbf5691c724bd8347b236
James Garinger <jamesg@Jamess-MacBook-Air.local>
2023-07-03 20:55:03 -0500

    adding cart page quantity add and remove button functionality

--- styletrackersite/pages/cart.js
+++ styletrackersite/pages/cart.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 
 export default function CartPage() {
-    const { cartProducts } = useContext(CartContext);
+    const { cartProducts, addProduct, removeProduct } = useContext(CartContext);
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
@@ -14,26 +14,71 @@ export default function CartPage() {
                     setProducts(res.data);
                 })
         }
-    }, [cartProducts])
+    }, [cartProducts]);
+
+    function addProductQuantity(productId) {
+        addProduct(productId);
+    };
+
+    function removeProductQuantity(productId) {
+        removeProduct(productId)
+    };
+
+    let total = 0;
+    for (const productId of cartProducts) {
+        const price = products.find(p => p._id === productId)?.price || 0;
+        total += price;
+    }
+
+    
 
     return (
         <>
             <Header />
             <div className="flex items-center sm:items-start flex-col sm:flex-row sm:m-10 gap-6 min-h-[300px]">
                 <div className="mt-8 sm:mt-0 w-[90%] sm:w-[60%] h-full bg-white rounded-lg p-[30px]">
+                    <h2 className="mb-6 font-bold text-[1.3em]">Cart </h2>
+
                     {!cartProducts?.length && (
                         <div className="">
                             Your Cart is Empty!
                         </div>
                     )}
                     {products?.length > 0 && (
-                        <div>
-                            <h2>Cart Items: </h2>
-                            {products.map(product => (
-                                <h4>{product.title}</h4>
-                            ))}
-                        </div>
+
+                        <table className="w-full">
+                            <thead className="uppercase text-left text-[#bbb] text-[.8rem]">
+                                <tr className="border-b-[2px] border-gray-300">
+                                    <th>Product</th>
+                                    <th>Quantity</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {products.map(product => (
+                                    <tr>
+                                        <td>
+                                            {product.title}
+                                            <div className="max-h-[80px] max-w-[80px] flex items-center justify-center p-2 bg-white shadow-md m-2 border-[1px] border-gray-300 shadow-[#666] rounded-md">
+                                                <img className="max-w-full max-h-[70px]" src={product.images[0]} />
+                                            </div>
+                                        </td>
+                                        <td className="">
+                                            <button onClick={() => removeProductQuantity(product._id)} className="bg-gray-200 rounded-lg px-3">-</button>
+                                            <span className="mx-2">{cartProducts.filter(id => id === product._id).length}</span>
+                                            <button onClick={() => addProductQuantity(product._id)} className="bg-gray-200 rounded-lg px-3">+</button>
+                                        </td>
+                                        <td>{cartProducts.filter(id => id === product._id).length * product.price}</td>
+
+                                    </tr>
+                                ))}
+                                <tr className="h-[120px] flex items-center font-bold">Total Price: {total.toFixed(2)} </tr>
+
+                            </tbody>
+                        </table>
                     )}
+
+
 
 
 
